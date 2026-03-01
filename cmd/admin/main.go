7d168e3989898677399024f1eb9c78b41523e365
uriwang <uriwang1986@gmail.com>
2026-03-01 12:52:38 +0800

    feat(admin): add admin service skeleton
    - Add cmd/admin and internal/admin (config, service, run, cmds, commands, apm)
    - No gRPC/proto; Run blocks after InitLogger, APM, RegisterSelf until Close
    - Subcommands: start, stop, restart, reload, health, version (supervisor + pid file)
    - Add etc/admin.yaml; extend Makefile BINARIES with admin

---
+++ cmd/admin/main.go
@@ -0,0 +1,58 @@
+/**
+ * Copyright 2025 Saber authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+**/
+
+package main
+
+import (
+	"os"
+
+	"os-artificer/saber/internal/admin"
+	"os-artificer/saber/pkg/logger"
+
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	if os.Getenv("SABER_ADMIN_SUPERVISOR") == "1" {
+		if err := admin.RunSupervisor(); err != nil {
+			os.Exit(1)
+		}
+		os.Exit(0)
+		return
+	}
+
+	rootCmd := &cobra.Command{
+		Use:          "Admin",
+		Short:        "Saber Admin Server",
+		SilenceUsage: true,
+		RunE:         admin.Run,
+	}
+
+	rootCmd.PersistentFlags().StringVarP(&admin.ConfigFilePath, "config", "c", "./etc/admin.yaml", "")
+	rootCmd.CompletionOptions.DisableDefaultCmd = true
+
+	rootCmd.AddCommand(admin.StartCmd)
+	rootCmd.AddCommand(admin.StopCmd)
+	rootCmd.AddCommand(admin.RestartCmd)
+	rootCmd.AddCommand(admin.ReloadCmd)
+	rootCmd.AddCommand(admin.HealthCheckCmd)
+	rootCmd.AddCommand(admin.VersionCmd)
+
+	if err := rootCmd.Execute(); err != nil {
+		logger.Errorf("failed to start admin server. errmsg:%s", err.Error())
+		return
+	}
+}
---
+++ internal/admin/config/config.go
@@ -0,0 +1,114 @@
+/**
+ * Copyright 2025 Saber authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+**/
+
+package config
+
+import (
+	"time"
+
+	"os-artificer/saber/pkg/logger"
+	"os-artificer/saber/pkg/sbnet"
+)
+
+var Cfg = Configuration{
+	Name:    "Admin",
+	Version: "v1.0.0",
+
+	Discovery: DiscoveryConfig{
+		EtcdEndpoint:          "http://etcd-server:2379",
+		EtcdUser:              "root",
+		EtcdPassword:          "sabertest",
+		DialTimeout:           5 * time.Second,
+		AutoSyncInterval:      10 * time.Second,
+		DialKeepAliveTime:     5 * time.Second,
+		DialKeepAliveTimeout:  5 * time.Second,
+		RegistryRootKeyPrefix: "os-artificer/saber",
+		RegistryTTL:           60,
+	},
+
+	APM: APMConfig{
+		Enabled: true,
+		Endpoint: sbnet.Endpoint{
+			Protocol: "http",
+			Host:     "127.0.0.1",
+			Port:     8200,
+		},
+	},
+
+	Service: ServiceConfig{
+		ListenAddress: sbnet.Endpoint{
+			Protocol: "tcp",
+			Host:     "127.0.0.1",
+			Port:     26690,
+		},
+	},
+
+	Log: LogConfig{
+		FileName:       "./logs/admin.log",
+		LogLevel:       logger.DebugLevel,
+		FileSize:       100,
+		MaxBackupCount: 10,
+		MaxBackupAge:   10,
+	},
+}
+
+// DiscoveryConfig discovery's config
+type DiscoveryConfig struct {
+	EtcdEndpoint          string        `yaml:"etcdEndpoint"`
+	EtcdUser              string        `yaml:"etcdUser"`
+	EtcdPassword          string        `yaml:"etcdPassword"`
+	DialTimeout           time.Duration `yaml:"dialTimeout"`
+	AutoSyncInterval      time.Duration `yaml:"autoSyncInterval"`
+	DialKeepAliveTime     time.Duration `yaml:"dialKeepAliveTime"`
+	DialKeepAliveTimeout  time.Duration `yaml:"dialKeepAliveTimeout"`
+	RegistryRootKeyPrefix string        `yaml:"registryRootKeyPrefix"`
+	RegistryTTL           int64         `yaml:"registryTTL"` // in seconds
+	UseTLS                bool          `yaml:"useTLS"`
+	InsecureSkipVerify    bool          `yaml:"insecureSkipVerify"`
+	EtcdCACert            string        `yaml:"etcdCACert"`
+	EtcdCert              string        `yaml:"etcdCert"`
+	EtcdKey               string        `yaml:"etcdKey"`
+}
+
+// APMConfig APM config
+type APMConfig struct {
+	Enabled  bool           `yaml:"enabled"`
+	Endpoint sbnet.Endpoint `yaml:"endpoint"`
+}
+
+// ServiceConfig service local config
+type ServiceConfig struct {
+	ListenAddress sbnet.Endpoint `yaml:"listenAddress"`
+}
+
+// LogConfig log config
+type LogConfig struct {
+	FileName       string       `yaml:"fileName"`
+	LogLevel       logger.Level `yaml:"logLevel"`
+	FileSize       int          `yaml:"fileSize"`
+	MaxBackupCount int          `yaml:"maxBackupCount"`
+	MaxBackupAge   int          `yaml:"maxBackupAge"`
+}
+
+// Configuration admin's configuration
+type Configuration struct {
+	Name      string          `yaml:"name"`
+	Version   string          `yaml:"version"`
+	Discovery DiscoveryConfig `yaml:"discovery"`
+	APM       APMConfig       `yaml:"apm"`
+	Service   ServiceConfig   `yaml:"service"`
+	Log       LogConfig       `yaml:"log"`
+}
